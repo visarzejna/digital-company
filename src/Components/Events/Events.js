e64a3c23ad34ba57db8d133412f89afac16ff8ba
albrimb7 <albrimb7@gmail.com>
2022-07-03 17:35:11 +0200

    added styling to buttons

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -10,8 +10,8 @@ const Events = () => {
             <h1 className={classes.title}>Upcoming Events and shows</h1>
             <p className={classes.parag}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
-              aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
+            </p>
           </div>
           <div className={classes.events}>
             <ul className={classes.uls}>
